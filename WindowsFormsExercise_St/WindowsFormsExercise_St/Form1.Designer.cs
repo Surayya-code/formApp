18144e85a5f53291689eafd1a9354cc9699c20c6
Surayya-code <surayyaah@code.edu.az>
2019-10-30 17:10:02 +0400

    form

---
+++ WindowsFormsExercise_St/WindowsFormsExercise_St/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsExercise_St
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxSiyahi = new System.Windows.Forms.RichTextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btbSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBoxSiyahi
+            // 
+            this.richTextBoxSiyahi.Location = new System.Drawing.Point(3, 12);
+            this.richTextBoxSiyahi.Name = "richTextBoxSiyahi";
+            this.richTextBoxSiyahi.Size = new System.Drawing.Size(462, 234);
+            this.richTextBoxSiyahi.TabIndex = 0;
+            this.richTextBoxSiyahi.Text = "";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(488, 25);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(151, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Location = new System.Drawing.Point(488, 65);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(151, 20);
+            this.textBoxSurname.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(551, 102);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(551, 144);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Delete";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btbSave
+            // 
+            this.btbSave.Location = new System.Drawing.Point(551, 184);
+            this.btbSave.Name = "btbSave";
+            this.btbSave.Size = new System.Drawing.Size(75, 23);
+            this.btbSave.TabIndex = 5;
+            this.btbSave.Text = "Save";
+            this.btbSave.UseVisualStyleBackColor = true;
+            this.btbSave.Click += new System.EventHandler(this.btbSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btbSave);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textBoxSurname);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.richTextBoxSiyahi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxSiyahi;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btbSave;
+    }
+}
+
---
+++ WindowsFormsExercise_St/WindowsFormsExercise_St/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsExercise_St
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string name = textBoxName.Text;
+            string surname = textBoxSurname.Text;
+
+            string fullName = name +" "+ surname;
+
+            richTextBoxSiyahi.Text=fullName;
+
+        }
+
+        private void btbSave_Click(object sender, EventArgs e)
+        {
+            string fileName = "Students.txt";
+            File.AppendAllText(fileName, $"{richTextBoxSiyahi.Text}{Environment.NewLine}");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string fileName = "Students.txt";
+            if(File.Exists(fileName))
+            {
+            richTextBoxSiyahi.Text= File.ReadAllText(fileName);
+            }
+           
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            string fileName = "Students.txt";
+            File.Delete(fileName);
+        }
+    }
+}
